992e88478b09d26162c56f212efe1df78945a8ad
LouAntonio <lourencodala88@gmail.com>
2025-11-04 13:14:06 +0100

    SPan class added

--- ex01/includes/Span.hpp
+++ ex01/includes/Span.hpp
@@ -6,7 +6,36 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:47:45 by lantonio          #+#    #+#             */
-/*   Updated: 2025/11/04 12:47:51 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/11/04 13:13:39 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <exception>
+
+class Span {
+	private:
+		unsigned int	n;
+	public:
+		Span();
+		Span(unsigned int value);
+		Span(const Span &src);
+		Span &operator=(const Span &src);
+
+		void	addNumber(void);
+		Span	shortestSpan(void);
+		Span	longestSpan(void);
+
+		class FullSpan : public std::exception {
+			virtual const char* what() const throw();
+		};
+
+		class EmptySpan : public std::exception {
+			virtual const char* what() const throw();
+		};
+};
+
+#endif
