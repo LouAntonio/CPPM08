7a9ad734e7cc11226632e51c4ef5222470de466d
LouAntonio <lourencodala88@gmail.com>
2025-11-21 15:31:03 +0100

    MutantStack base setup and subject tests

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 10:20:23 by lantonio          #+#    #+#             */
-/*   Updated: 2025/11/21 14:52:35 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/11/21 15:28:20 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,28 @@
 
 #include <iostream>
 #include <stack>
+#include <list>
+#include <algorithm>
 
 template <typename T>
 class MutantStack : public std::stack<T>{
 public:
-	MutantStack();
-	MutantStack(const MutantStack &src) : std::stack<T>(src);
-	MutantStack &operator=(const MutantStack &src);
-	~MutantStack();
+	MutantStack() {};
+	MutantStack(const MutantStack &src) : std::stack<T>(src) {};
+	MutantStack &operator=(const MutantStack &src) {
+		if (this != &src)
+			std::stack<T>::operator=(src);
+		return *this;
+	};
+	~MutantStack() {};
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-	iterator begin(void);
-	iterator end(void);
-
-	const_iterator begin(void);
-	const_iterator end(void);
+	iterator		begin(void) { return this->c.begin(); }
+	iterator		end(void) { return this->c.end(); }
+	const_iterator	begin(void) const { return this->c.begin(); }
+	const_iterator	end(void) const { return this->c.end(); }
 };
-
-#include "../src/MutantStack.tpp"
 
 #endif
