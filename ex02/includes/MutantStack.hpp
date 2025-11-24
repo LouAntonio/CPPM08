a510c15a2d4dd6e699cdee2b138d138dcc4eaa35
LouAntonio <lourencodala88@gmail.com>
2025-11-24 11:02:30 +0100

    reverse iterator tests

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 10:20:23 by lantonio          #+#    #+#             */
-/*   Updated: 2025/11/21 15:28:20 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/11/21 15:45:22 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,20 @@ public:
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator const_reverse_iterator;
 
-	iterator		begin(void) { return this->c.begin(); }
-	iterator		end(void) { return this->c.end(); }
-	const_iterator	begin(void) const { return this->c.begin(); }
-	const_iterator	end(void) const { return this->c.end(); }
+	iterator				begin(void) { return this->c.begin(); }
+	iterator				end(void) { return this->c.end(); }
+
+	const_iterator			begin(void) const { return this->c.begin(); }
+	const_iterator			end(void) const { return this->c.end(); }
+
+	reverse_iterator		rbegin(void) { return this->c.rbegin(); }
+	reverse_iterator		rend(void) { return this->c.rend(); }
+
+	const_reverse_iterator	rbegin(void) const { return this->c.rbegin(); }
+	const_reverse_iterator	rend(void) const { return this->c.rend(); }
 };
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 14:55:02 by lantonio          #+#    #+#             */
-/*   Updated: 2025/11/21 15:29:09 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/11/24 11:01:40 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,36 @@ int main() {
 			++it;
 		}
 		std::stack<int> s(mstack);
+		std::cout << "---" << std::endl;
+	}
+	{
+		MutantStack<int> m;
+		m.push(1);
+		m.push(2);
+		m.push(3);
+		m.push(42);
+
+		std::cout << "Top = " << m.top() << std::endl;
+		std::cout << "Size = " << m.size() << std::endl;
+
+		MutantStack<int> n(m);
+		MutantStack<int>::iterator i = m.begin();
+		MutantStack<int>::iterator j = m.end();
+		while (i != j)
+		{
+			std::cout << *i << " ";
+			++i;
+		}
+		std::cout << std::endl;
+		MutantStack<int> o;
+		o = n;
+		MutantStack<int>::reverse_iterator k = o.rbegin();
+		MutantStack<int>::reverse_iterator l = o.rend();
+		while (k != l)
+		{
+			std::cout << *k << " ";
+			++k;
+		}
+		std::cout << std::endl;
 	}
 }
